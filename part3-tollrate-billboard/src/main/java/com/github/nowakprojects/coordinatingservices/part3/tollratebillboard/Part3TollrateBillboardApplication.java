8cc98a0a57fc2e46dbade60ceb4e1dde62d3c0e8
mnowak <mateusz.nowak@intenso.pl>
2019-05-14 20:56:52 +0200

    Part3 Consuming services with load balancing.

---
+++ part3-tollrate-billboard/src/main/java/com/github/nowakprojects/coordinatingservices/part3/tollratebillboard/Part3TollrateBillboardApplication.java
@@ -0,0 +1,25 @@
+package com.github.nowakprojects.coordinatingservices.part3.tollratebillboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class Part3TollrateBillboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Part3TollrateBillboardApplication.class, args);
+    }
+
+
+    @LoadBalanced
+    @Bean
+    RestTemplate restTemplate(RestTemplateBuilder builder){
+        return builder.build();
+    }
+}
